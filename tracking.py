cde4f14c7c995336792117488990b61533843a31
Joris GUERRY <joguerry+git@gmail.com>
2018-06-26 15:31:28 +0200

    wd

--- tracking.py
+++ tracking.py
@@ -5,33 +5,41 @@ import time
 (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
 
 
-def compute_iou(box, boxes, box_area, boxes_area):
-    """Calculates IoU of the given box with the array of the given boxes.
-    box: 1D vector [y1, x1, y2, x2]
-    boxes: [boxes_count, (y1, x1, y2, x2)]
-    box_area: float. the area of 'box'
-    boxes_area: array of length boxes_count.
+def bb_intersection_over_union(boxA, boxB):
+	# determine the (x, y)-coordinates of the intersection rectangle
+	xA = max(boxA[0], boxB[0])
+	yA = max(boxA[1], boxB[1])
+	xB = min(boxA[2], boxB[2])
+	yB = min(boxA[3], boxB[3])
 
-    Note: the areas are passed in rather than calculated here for
-          efficency. Calculate once in the caller to avoid duplicate work.
-    """
-    # Calculate intersection areas
-    y1 = np.maximum(box[0], boxes[:, 0])
-    y2 = np.minimum(box[2], boxes[:, 2])
-    x1 = np.maximum(box[1], boxes[:, 1])
-    x2 = np.minimum(box[3], boxes[:, 3])
-    intersection = np.maximum(x2 - x1, 0) * np.maximum(y2 - y1, 0)
-    union = box_area + boxes_area[:] - intersection[:]
-    iou = intersection / union
-    return iou
+	# compute the area of intersection rectangle
+	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
 
+	# compute the area of both the prediction and ground-truth
+	# rectangles
+	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
 
-class face_detector:
-    def __init__(self,time_wait=1):
+	# compute the intersection over union by taking the intersection
+	# area and dividing it by the sum of prediction + ground-truth
+	# areas - the interesection area
+	iou = interArea / float(boxAArea + boxBArea - interArea)
+
+	# return the intersection over union value
+	return iou
+
+class face_brain:
+    def __init__(self,time_wait=5):
         self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         self.faces = []
         self.last_update = 0
         self.time_wait = time_wait
+        self.used = False
+
+
+        self.trackers = []
+        self.tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
+        self.tracker_type = self.tracker_types[1]
 
     def detect_face(self,frame):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -43,19 +51,18 @@ class face_detector:
             print("Updating faces")
             self.faces = self.detect_face(frame)
             self.last_update = time.time()
+            self.used = False
 
-class face_trackers:
-    def __init__(self):
-        self.trackers = []
-            # Set up tracker.
-            # Instead of MIL, you can also use
-
-        tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
-        self.tracker_type = tracker_types[1]
+    def get_faces(self):
+        if self.used:
+            return []
+        else:
+            self.used = True
+            return self.faces
 
     def add_tracker(self,frame,bbox):
         if int(minor_ver) < 3:
-            tracker = cv2.Tracker_create(tracker_type)
+            tracker = cv2.Tracker_create(self.tracker_type)
         else:
             if self.tracker_type == 'BOOSTING':
                 tracker = cv2.TrackerBoosting_create()
@@ -69,18 +76,43 @@ class face_trackers:
                 tracker = cv2.TrackerMedianFlow_create()
             if self.tracker_type == 'GOTURN':
                 tracker = cv2.TrackerGOTURN_create()
-        selF.trackers.append({"tracker":tracker.init(frame, bbox)})
+        # print(bbox)
+        tracker.init(frame, tuple(bbox))
+        self.trackers.append({"tracker":tracker,"bbox":bbox})
 
     def update_trackers(self,frame):
         for t in self.trackers:
             t['ok'], t['bbox'] = t['tracker'].update(frame)
+        self.trackers = [t for t in self.trackers if t['ok']]
+
+    def print_status(self,text="",debug=False):
+        if debug:
+            print("Nombre de trackers :", len(self.trackers))
+            print("Nombre de faces    :", len(self.faces))
+            print(40*"-")
 
     def already_tracked(self,bbox):
         for t in self.trackers:
-            joris il faut rajouter le calcul des aires (voir dans maskrcnn)
-            if compute_iou(bbox,t['bbox'])>0.8:
+            iou=bb_intersection_over_union(bbox,t['bbox'])
+            print(iou)
+            if iou>0.5:
                 return True
         return False
+
+    def update_frame(self,frame):
+        self.print_status("0 : update_trackers")
+        self.update_trackers(frame)
+        self.print_status("1 : updates_faces")
+        self.update_faces(frame)
+        self.print_status("2 : already tracked ?")
+        for face in self.get_faces():
+            print("About a face :",face)
+            if not self.already_tracked(face):
+                print("new tracking")
+                self.add_tracker(frame,face)
+            else:
+                print("already tracked")
+
 
 
 if __name__ == '__main__' :
@@ -123,8 +155,7 @@ if __name__ == '__main__' :
     # Initialize tracker with first frame and bounding box
     # ok = tracker.init(frame, bbox)
 
-    fd = face_detector()
-    ft = face_trackers()
+    fb = face_brain()
     while True:
 
         # Start timer
@@ -138,20 +169,8 @@ if __name__ == '__main__' :
             break
 
 
-        ft.update_trackers(frame)
+        fb.update_frame(frame)
 
-
-        fd.update_faces(frame)
-
-
-        for face in faces:
-            if face not already tracked:
-                add tracker
-            else:
-                pass
-        if bbox is None:
-            if len(fd.faces)>0:
-                print("faces here:",fd.faces)
 
         # Update tracker
         # ok, bbox = tracker.update(frame)
@@ -160,12 +179,19 @@ if __name__ == '__main__' :
         # Calculate Frames per second (FPS)
         fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
 
+
+        for id_t,t in enumerate(fb.trackers):
+
+            # print("drawing",t)
+            bbox = t['bbox']
         # Draw bounding box
         # if ok:
             # Tracking success
-            # p1 = (int(bbox[0]), int(bbox[1]))
-            # p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            # cv2.rectangle(frame, p1, p2, (255,255,0), 2, 1)
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255,255,0), 2, 1)
+            p1_text = (int(bbox[0])+5, int(bbox[1]))
+            cv2.putText(frame, str(id_t), p1_text, cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,255,255),2)
         # else :
             # Tracking failure
             # cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
